a4655e45e8fdf5dd87c72d9f79ad64ae399e1db1
Shahadat Hossain <shahadat.cseru@gmail.com>
2019-07-02 17:11:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const uuid4 = require('uuid4')
+const port = 7000
+
+
+let counter = 0
+
+app.get('/api/sms/', (req, res) => {
+
+  const DT = new Date()
+  console.log(DT,req.originalUrl)
+
+  const id = uuid4()
+  const mobile = '01717541865'
+  const body = 'This is sms number ' + counter++
+
+  console.log(`${id} ${mobile} ${body}`)
+  
+  res.send(`${id} ${mobile} ${body}`)
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
