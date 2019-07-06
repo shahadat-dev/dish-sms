cfaaa2f2e74074217897bbefb24ecc6b8c5abc75
Shahadat Hossain <shahadat.cseru@gmail.com>
2019-07-07 05:12:42 +0600

    remove keys_dev from repo

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: `mongodb://root:mati010107@ds229786-a0.gpj65.fleet.mlab.com:29786,ds229786-a1.gpj65.fleet.mlab.com:29786/dishbill?replicaSet=rs-ds229786&ssl=true`,
-  apiKey: 'IwiBGoWbhE5aLM9epbzPkCyz6Hi94pr9'
-}
